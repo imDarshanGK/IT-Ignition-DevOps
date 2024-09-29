e51870264e8de963f96b442c8557083cf962c936
imDarshanGK <darshan241210s@gmail.com>
2024-09-29 14:23:37 +0530

    Restored deleted files

---
+++ Introduction to Python and Basic Syntax/add_numbers.py
@@ -0,0 +1,15 @@
+# Task
+# Objective
+# LAB 3 : Write a Python function add_numbers(num1, num2) that takes two integers as input and returns their sum.
+# Input:
+# The function takes two integers num1 and num2.
+# Output:
+# The function returns the sum of num1 and num2.
+
+# Constraint
+# num1 and num2 will be integers between -100000 and 100000.
+
+def add_numbers(num1, num2):
+    return num1 + num2
+result = add_numbers(10, 20)
+print(result)
