544fc9e3ffe940f70c8e41080887d009b217a9e6
imDarshanGK <darshan241210s@gmail.com>
2024-09-30 20:35:37 +0530

    Added create_data_structures.py and other files

---
+++ Day 4/Data Structures/create_data_structures.py
@@ -0,0 +1,27 @@
+# Task
+# Objective
+# LAB 1 :
+# Write a Python function create_data_structures() that performs the following tasks:
+
+# 1. Create a list called my_list containing the numbers [1, 2, 3].
+# 2. Create a tuple called my_tuple containing the strings ("apple", "banana", "cherry").
+# 3. Create a dictionary called my_dict with keys as {"name": "Alice", "age": 25}.
+# 4. Create a set called my_set with values {1, 2, 3}.
+
+# Your function should return all four data structures as a tuple in the order: (list, tuple, dictionary, set).
+
+# Constraint
+# The list will contain exactly 3 integers.
+# The tuple will contain exactly 3 strings.
+# The dictionary will have two key-value pairs: one string (name) and one integer (age).
+# The set will contain 3 unique integers.
+
+
+def create_data_structures():
+    my_list =  [1, 2, 3]
+    my_tuple = ("apple", "banana", "cherry")
+    my_dict = {"name": "Alice", "age": 25}
+    my_set =  {1, 2, 3}
+    return my_list, my_tuple, my_dict, my_set
+data = create_data_structures()
+print(data)
---
+++ Day 4/Data Structures/get_second_color.py
@@ -0,0 +1,13 @@
+# Task
+# Objective
+# LAB 2 :
+# Write a Python function get_second_color(colors) that takes a tuple of colors as input and returns the second color in the tuple.
+
+# Constraint
+# The tuple will contain at least two colors, each as a string.
+
+def get_second_color(colors):
+    return colors[1]
+colors = ("red","blue","green")
+second_color = get_second_color(colors)
+print(second_color)
