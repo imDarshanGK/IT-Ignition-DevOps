f5e1429591648ea284acb1dc60da07c889940fab
imDarshanGK <darshan241210s@gmail.com>
2024-10-17 20:37:25 +0530

    Renamed 'Python and Basic Syntax' add number

--- Day 1 Introduction to Python and Basic Syntax/add_numbers.py
+++ Day 1 Introduction to Python and Basic Syntax/add_numbers.py
@@ -1,13 +1,16 @@
-# Task
-# Objective
-# LAB 3 : Write a Python function add_numbers(num1, num2) that takes two integers as input and returns their sum.
-# Input:
-# The function takes two integers num1 and num2.
-# Output:
-# The function returns the sum of num1 and num2.
+'''
+Task
+Objective
+LAB 3 : Write a Python function add_numbers(num1, num2) that takes two integers as input and returns their sum.
+Input:
+The function takes two integers num1 and num2.
+Output:
+The function returns the sum of num1 and num2.
 
-# Constraint
-# num1 and num2 will be integers between -100000 and 100000.
+Constraint
+num1 and num2 will be integers between -100000 and 100000.
+'''
+
 
 def add_numbers(num1, num2):
     return num1 + num2
