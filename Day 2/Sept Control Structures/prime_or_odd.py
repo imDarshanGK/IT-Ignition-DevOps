51445a93d7e9a0e1f2abb1e440859fa70f0fd6e5
imDarshanGK <darshan241210s@gmail.com>
2024-10-17 21:48:11 +0530

    Update commit: Prime number(Even or Odd)

--- Day 2/Sept Control Structures/prime_or_odd.py
+++ Day 2/Sept Control Structures/prime_or_odd.py
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
-# Task
-# Objective
-# LAB 1 : Write a Python function is prime_or_even_odd(n) that takes an integer n as input and performs the following:
+'''
+Task
+Objective
+LAB 1 : Write a Python function is prime_or_even_odd(n) that takes an integer n as input and performs the following:
 
-# If n is a prime number, return True.
-# If n is not a prime number, return "Even" if n is even, or "Odd" if n is odd. 
+If n is a prime number, return True.
+If n is not a prime number, return "Even" if n is even, or "Odd" if n is odd. 
 
-# Constraint
-# The input will be an integer between 1 and 1000. 
-
-
-def is_prime_or_even_odd(n):
-    if n < 2:
-        return False #not prime
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return "Even" if n % 2 == 0 else "Odd"
-=======
-# Task
-# Objective
-# LAB 1 : Write a Python function is prime_or_even_odd(n) that takes an integer n as input and performs the following:
-
-# If n is a prime number, return True.
-# If n is not a prime number, return "Even" if n is even, or "Odd" if n is odd. 
-
-# Constraint
-# The input will be an integer between 1 and 1000. 
+Constraint
+The input will be an integer between 1 and 1000. 
+'''
 
 
 def is_prime_or_even_odd(n):
@@ -34,5 +17,4 @@ def is_prime_or_even_odd(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return "Even" if n % 2 == 0 else "Odd"
->>>>>>> 2a56d59ae7ee7bfb256aeb7f68efbdeb6709b953
     return True #prime
