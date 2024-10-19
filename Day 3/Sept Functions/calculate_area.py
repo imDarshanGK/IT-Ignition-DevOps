a9f4389ccca9adeac3fafa4a123d780eec97af70
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 18:58:50 +0530

    update commit: Area of calculate length, width

--- Day 3/Sept Functions/calculate_area.py
+++ Day 3/Sept Functions/calculate_area.py
@@ -1,10 +1,12 @@
-# Task
-# Objective
-# LAB 2 :
-# Write a Python function calculate_area(length, width) that takes two inputs: two integers length and width, and returns the area of a rectangle.
+'''
+Task
+Objective
+LAB 2 :
+Write a Python function calculate_area(length, width) that takes two inputs: two integers length and width, and returns the area of a rectangle.
 
-# Constraint
-# Both length and width will be positive integers between 1 and 1000.
+Constraint
+Both length and width will be positive integers between 1 and 1000.
+'''
 
 
 def calculate_area(length, width):
