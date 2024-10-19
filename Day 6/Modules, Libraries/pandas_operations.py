61a5a6cb10d692ac53b186f92df70d5be6cfaf02
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 19:19:35 +0530

    Pandas operations filtered DataFrame and sum of ages.

--- Day 6/Modules, Libraries/pandas_operations.py
+++ Day 6/Modules, Libraries/pandas_operations.py
@@ -1,15 +1,17 @@
-# Task
-# Objective
-# LAB 5 : Write a Python function pandas_operations() -> dict that performs the following operations using the Pandas library:
-# Create a DataFrame with the following data:
-# {"Name": ["Alice", "Bob", "Charlie"], "Age": [25, 30, 35]}
-# Filter the DataFrame to include only rows where the Age is greater than 28.
-# Calculate the sum of the Age column.
-# The function should return a dictionary containing the filtered DataFrame and the sum of ages.
+'''
+Task
+Objective
+LAB 5 : Write a Python function pandas_operations() -> dict that performs the following operations using the Pandas library:
+Create a DataFrame with the following data:
+{"Name": ["Alice", "Bob", "Charlie"], "Age": [25, 30, 35]}
+Filter the DataFrame to include only rows where the Age is greater than 28.
+Calculate the sum of the Age column.
+The function should return a dictionary containing the filtered DataFrame and the sum of ages.
 
-# Constraint
-# Use the pandas library for data manipulation.
-# The function should not accept any arguments.
+Constraint
+Use the pandas library for data manipulation.
+The function should not accept any arguments.
+'''
 
 
 import pandas as pd
