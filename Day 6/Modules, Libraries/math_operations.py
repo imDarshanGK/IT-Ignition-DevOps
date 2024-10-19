108d4dfff4b75fe7ae79e50e8b00d81f9e5ae12e
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 19:16:39 +0530

    Math operations - square root, sine, factorial

--- Day 6/Modules, Libraries/math_operations.py
+++ Day 6/Modules, Libraries/math_operations.py
@@ -1,16 +1,17 @@
-# Task
-# Objective
-# LAB 1 :
-# Write a Python function math_operations(num: int) -> dict that takes a user input  integer num and returns a dictionary containing:
-# The square root of num.
-# The sine of 90 degrees.
-# The factorial of num.
+'''
+Task
+Objective
+LAB 1 :
+Write a Python function math_operations(num: int) -> dict that takes a user input  integer num and returns a dictionary containing:
+The square root of num.
+The sine of 90 degrees.
+The factorial of num.
 
-# Constraint
-# num will be a non-negative integer between 1 and 100.
-# For sine calculation, convert degrees to radians using the math.radians() function.
-# Use the math module to perform these operations.
-
+Constraint
+num will be a non-negative integer between 1 and 100.
+For sine calculation, convert degrees to radians using the math.radians() function.
+Use the math module to perform these operations.
+'''
 
 import math
 def math_operations(num: int) -> dict:
