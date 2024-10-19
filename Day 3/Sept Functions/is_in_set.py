b2db269ad91e231694f121dd1ac0d3fe8f95830e
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 19:01:07 +0530

    update commit: Is in set my_set, item

---
+++ Day 3/Sept Functions/is_in_set.py
@@ -0,0 +1,12 @@
+'''
+Task
+Objective
+LAB 4 :
+Write a Python function is_in_set(my_set, item) that takes a set my_set and a variable item. The function should return True if the item exists in the set, otherwise return False.
+
+Constraint
+The set will contain up to 10 elements.
+'''
+
+def is_in_set(my_set, item):
+    return item in my_set
