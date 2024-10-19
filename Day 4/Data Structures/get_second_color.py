81935e11fa621d372ef25b20afe174742181eb07
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 19:05:56 +0530

    update commit: Get second color colors in tuple

--- Day 4/Data Structures/get_second_color.py
+++ Day 4/Data Structures/get_second_color.py
@@ -1,10 +1,12 @@
-# Task
-# Objective
-# LAB 2 :
-# Write a Python function get_second_color(colors) that takes a tuple of colors as input and returns the second color in the tuple.
+'''
+Task
+Objective
+LAB 2 :
+Write a Python function get_second_color(colors) that takes a tuple of colors as input and returns the second color in the tuple.
 
-# Constraint
-# The tuple will contain at least two colors, each as a string.
+Constraint
+The tuple will contain at least two colors, each as a string.
+'''
 
 def get_second_color(colors):
     return colors[1]
