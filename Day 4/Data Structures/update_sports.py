3e48028906bc717b6b2ed1cb6947afb6dd8448b2
imDarshanGK <darshan241210s@gmail.com>
2024-10-19 19:07:20 +0530

    update commit: Update sports add cricket and remove tennis

--- Day 4/Data Structures/update_sports.py
+++ Day 4/Data Structures/update_sports.py
@@ -1,10 +1,12 @@
-# Task
-# Objective
-# LAB 4 :
-# Write a Python function update_sports(sports) that takes a set of sports, adds "cricket" to it, removes "tennis", and returns the updated set.
+'''
+Task
+Objective
+LAB 4 :
+Write a Python function update_sports(sports) that takes a set of sports, adds "cricket" to it, removes "tennis", and returns the updated set.
 
-# Constraint
-# The set will have a maximum of 5 sports.
+Constraint
+The set will have a maximum of 5 sports.
+'''
 
 def update_sports(sports):
     sports.add("cricket")
